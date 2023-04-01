7592d91aa1e1a47f7dca04e32f16e18fad319d1d
Uriel Solís <euss99@hotmail.com>
2023-03-31 23:47:35 -0600

    Modified path cv

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -6,7 +6,7 @@ const translations: { en: Translation; es: Translation } = {
       link1: "Home",
       link2: "Projects",
       link3: "Contact",
-      routePath: "/public/Resume-EfrainUrielSolisSalinas.pdf"
+      routePath: "/Resume-EfrainUrielSolisSalinas.pdf"
     },
     heroSection: {
       title: "I'm Uriel Solís",
@@ -139,7 +139,7 @@ const translations: { en: Translation; es: Translation } = {
       link1: "Inicio",
       link2: "Proyectos",
       link3: "Contacto",
-      routePath: "/public/CV-EfrainUrielSolisSalinas.pdf"
+      routePath: "/CV-EfrainUrielSolisSalinas.pdf"
     },
     heroSection: {
       title: "Soy Uriel Solís",
